45e3a20efb8498e233bf5b1dfa1174de79739590
Nathan Gingrich <njgingrich@gmail.com>
2019-11-24 18:14:38 -0500

    Add basic group adding

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,29 @@ import React from 'react';
 import AddTodo from 'features/todos/components/AddTodo';
 import VisibleTodoList from 'features/todos/components/VisibleTodoList';
 import Filters from 'features/filters/components/Filters';
-import StatusBar from './StatusBar';
+import AddGroup from 'features/groups/components/AddGroup';
+import TodoGroupList from 'features/groups/components/TodoGroupList';
 
+import StatusBar from 'components/StatusBar';
 import '../styles/application.css';
 
 export function App() {
   return (
     <main className="page-layout">
-      <section className="content">
+      <section className="groups-section">
+        <TodoGroupList className="groups-list" />
+        <div className="groups-add">
+          <AddGroup />
+        </div>
+      </section>
+      <section className="todos-section">
         <AddTodo />
         <VisibleTodoList />
         <Filters />
       </section>
-      <section className="status-bar">
+      <footer className="status-bar">
         <StatusBar />
-      </section>
+      </footer>
     </main>
   );
 }
